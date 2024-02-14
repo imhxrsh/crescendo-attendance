5e30d5b0f0acc89ca5a271345d36a7aa49156bbe
Harsh <itsmehxrsh@gmail.com>
2024-02-14 18:59:11 +0530

    Update code formatting and fix minor bugs

---
+++ qr/qrcodegen.py
@@ -0,0 +1,12 @@
+import segno
+
+# Loop through each code and generate QR code
+for i in range(1, 3):
+    code = f"CRESE{i:03}"  # Format the code with leading zeros
+    qrcode = segno.make_qr(code)
+    qrcode.save(
+        f"{code}.png",  # Save with the code as part of the filename
+        scale=5,
+        light="white",
+        dark="lightgreen",
+    )
